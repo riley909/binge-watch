f8a9a1da3cae93aa89badaf181506ca4d7762e75
riley909 <increaseblue666@gmail.com>
2020-06-30 12:22:04 +0900

    formData에 대한 주ìì석 작ì성

--- js/applyComment.js
+++ js/applyComment.js
@@ -1,5 +1,11 @@
 $('#writeCommentBtn').click(function () {
+    // 폼 내부 태그 값들을 배열로 만든다
     var formData = $('#commentForm').serializeArray();
+    // formData[0] : 댓글내용
+    // formData[1] : 작성자
+    // formData[2] : 글 인덱스
+    // formData[3] : 댓글작성일
+    // formData[4] : 댓글작성시간
     console.log(formData);
     $.post($("#commentForm").attr("action"), formData, function(info){
         console.log(info);
