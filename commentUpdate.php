a981841c8a4182a3f1681b228e22f7274d636795
riley909 <increaseblue666@gmail.com>
2020-07-01 18:26:45 +0900

    -작성 후 새로고침 가능하게됨 -업데이트를 한번 하고 나면 작성은 가능한데 삭제하려고 할때 오류발생

--- commentUpdate.php
+++ commentUpdate.php
@@ -3,18 +3,20 @@ require 'menu/db.php';
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
+function console_log($data){
+    echo "<script>console.log('PHP_CONSOLE : ".$data."');</script>";
+}
+
 $reviewNum = $_POST['review_num'];
 //한 게시글 안의 모든 댓글을 출력하기 위해 해당 게시글 번호를 가진 모든 댓글을 조회한다
 $sql = queryResult("SELECT *FROM comments WHERE review_num = '$reviewNum'");
 //댓글 개수도 반영해야 하기 때문에 같이 조회함
 $totalCommentNum = mysqli_num_rows($sql);
-$row = $sql->fetch_array();
-//TODO: 여러개의 행을 전부 가져올수 있도록 수정하기
-$commentIdx = $row['idx'];
-$id = $row['id'];
-$commentContent = $row['comment_content'];
-$commentDate = $row['comment_date'];
-$commentTime = $row['comment_time'];
-echo(json_encode(array('comment_idx' => $commentIdx, 'id' => $id, 'comment_content' => $commentContent, 'comment_date' => $commentDate,
-    'comment_time' => $commentTime, 'total_comment_num' => $totalCommentNum)));
+$commentList = array();
+while ($row = $sql->fetch_assoc()) {
+    $commentList[] = $row;
+    $commentList['total_comment_num'] = $totalCommentNum;
+}
+echo json_encode($commentList);
+
 ?>
--- js/commentUpdate.js
+++ js/commentUpdate.js
@@ -6,22 +6,30 @@ function updateCommentList(myId, reviewNum) {
         data: {'review_num': reviewNum},
         dataType: 'json',
         success: function (result) {
-            console.log(result.length);
-            let id = result.id;
-            let idx = result.comment_idx;
-            let commentContent = result.comment_content;
-            let commentDate = result.comment_date;
-            let commentTime = result.comment_time;
-            let totalCommentNum = result.total_comment_num;
+            let resultArr = result;
+            console.log(resultArr);
+            // 댓글 목록이 중복 출력되는 것을 피하기 위해 목록 출력 전에 창을 초기화한다
+            $(".comments-list").empty();
+            let totalCommentNum = resultArr.total_comment_num;
+            console.log('total: ', totalCommentNum);
             // 총 댓글의 개수는 댓글의 삭제, 수정, 작성과 상관없이 항상 반영한다
-            $("#totalCommentNum").val(totalCommentNum);
-            for (let i = 0; i < result.length; i++) {
-                console.log(result);
+            // 텍스르에 접근하려면 .val() 이 아니라 .html()을 사용할 것
+            $("#totalCommentNum").html(totalCommentNum);
+            // 반복문에서는 필요 없는 정보인 총 댓글 개수를 배열에서 삭제한다
+            delete resultArr['total_comment_num'];
+            console.log('재확인: ', resultArr);
+            for (let i = 0; i < totalCommentNum; i++) {
+                let id = resultArr[i].id;
+                let idx = resultArr[i].idx;
+                let commentContent = resultArr[i].comment_content;
+                let commentDate = resultArr[i].comment_date;
+                let commentTime = resultArr[i].comment_time;
                 // id가 로그인된 것과 일치할 경우에만 수정, 삭제버튼 출력
-                if (myId === result[i].id) {
+                if (myId === resultArr[i].id) {
+                    let commentIdx = $('#commentIdx').val();
                     $(".comments-list").append("<div class=\"media\">"
                         + "<a class=\"media-left\" href=\"#\">"
-                        + "<img src=\"http://lorempixel.com/40/40/people/1/\">"
+                        // + "<img src=\"http://lorempixel.com/40/40/people/1/\">"
                         + "</a>"
                         + "<div class=\"media-body\">"
                         + "<h4 class=\"media-heading user_name\">" + id + "</h4>"
@@ -29,16 +37,16 @@ function updateCommentList(myId, reviewNum) {
                         + "<input type=\"hidden\" id=\"reviewNum\" value=" + reviewNum + "/>"
                         + "<input type=\"hidden\" id=\"commentIdx\" value=" + idx + "/>"
                         + "<p><small><button type=\"button\" style=\"border: 0; outline:0; color:blue;\">수정</button>"
-                        + "<button type=\"button\" style=\"border: 0; outline:0; color:blue;\" onclick=\"deleteComment()\">삭제"
+                        + "<button type=\"button\" style=\"border: 0; outline:0; color:blue;\" onclick=\"deleteComment(" + commentIdx + ")\">삭제"
                         + "</button></small></p></div>"
                         + "<p class=\"pull-right\">"
                         + "<small>" + commentDate + "</small><br>"
                         + "<small>" + commentTime + "</small>"
-                        + "</p> </div>");
+                        + "</p></div>");
                 } else {
                     $(".comments-list").append("<div class=\"media\">"
                         + "<a class=\"media-left\" href=\"#\">"
-                        + "<img src=\"http://lorempixel.com/40/40/people/1/\">"
+                        // + "<img src=\"http://lorempixel.com/40/40/people/1/\">"
                         + "</a>"
                         + "<div class=\"media-body\">"
                         + "<h4 class=\"media-heading user_name\">" + id + "</h4>"
@@ -48,7 +56,7 @@ function updateCommentList(myId, reviewNum) {
                         + "<p class=\"pull-right\">"
                         + "<small>" + commentDate + "</small><br>"
                         + "<small>" + commentTime + "</small>"
-                        + "</p> </div>");
+                        + "</p></div>");
                 }
             }
         }
